90bb073d071cdf7cd81ddc59e9dac3b6dac59cfd
Rodolfo Dornelas <rodolfo.dpo@gmail.com>
2021-07-22 21:10:26 -0300

    consegui dar uma cara, mas falta componentizar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './App.Style';
+import {ContainerProdutos} from './App.Style'
+import {CardProduto} from './App.Style'
+import {Footer} from "./App.Style"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <h1>esse é o header</h1>
+      </Header>
+      <ContainerProdutos   style={{
+          backgroundImage: `url(${require('./img/background.jpg')})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          }}>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+            <CardProduto>
+                <h3>Produto aqui</h3>
+            </CardProduto>
+      </ContainerProdutos>
+      <Footer>
+        <h1>esse é o footer</h1>
+      </Footer>
+
+
+
     </div>
   );
 }
